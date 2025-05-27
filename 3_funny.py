be260f9f6719fb68758a7751a225b882fbb1c5f9
zhaosong217 <53959511+zhaosong217@users.noreply.github.com>
2025-05-27 10:03:46 +0200

    Add files via upload

--- 3_funny.py
+++ 3_funny.py
@@ -1,50 +1,47 @@
-# %%
+import random
 import pandas as pd
-import os
 import time
+import os
 
-print('*'*50)
-current_time = time.strftime("%Y%m%d")
-print(f"Welcome back Jack! Today is {current_time}\nWhich file do you want to check?")
-# %%
-folder_path = '/Users/songzhao/Desktop/Eudic背单词/'
-file_list = os.listdir(folder_path)
-xlsx_lst = []
-for i in file_list:
-    if i[:6] == 'Eudic_':
-        xlsx_lst.append(i)
-for i in range(len(xlsx_lst)):
-    print(f"{i+1}: {xlsx_lst[i]}")
-# %%
-num = int(input('Plese Select a file to learn: '))
-# num = 5
-file = folder_path + xlsx_lst[num-1]
-df = pd.read_excel(file)
-print(f"{xlsx_lst[num-1][:-5]} has {len(df)} words.\nYou can do it!")
-# %%
-models = {1:'Order', 2:'Random', 3:'I want TEN!'}
-for k,v in models.items():
-    print(f"{k}: {v}")
-model = int(input('Please select the model: '))
-print('*'*50)
+def profile():
+    global current_time
+    current_time = time.strftime("%Y%m%d")
+    name = 'Jack'
+    print(f"Welcome back {name}! Today is {current_time}")
 
+def file_choice():
+    print("Which file do you want to check?")
+    global folder_path
+    folder_path = '/Users/songzhao/Desktop/Eudic背单词/'
+    file_list = os.listdir(folder_path)
+    xlsx_lst = []
+    for i in file_list:
+        if i[:6] == 'Eudic_':
+            xlsx_lst.append(i)
+    for i in range(len(xlsx_lst)):
+        print(f"{i+1}: {xlsx_lst[i]}")
+    num = int(input('Please Enter the Number: '))
+    global file
+    file = folder_path + xlsx_lst[num-1]
+    df = pd.read_excel(file)
+    print(f"{xlsx_lst[num-1][:-5]} has {len(df)} words.\nYou can do it!")
+    return file
 
-
-results = []
-# order model
 def order():
     for i in range(len(df['No.'])):
         print(f"{i+1}. {df.loc[i, '解释']}")
-        text = input("Enter your response:\n")
+        fill_blanks(df.loc[i,'单词'])
+        text = input('Your response: ')
         df.loc[i, current_time] = text
-        if text != df.loc[i, '单词']:
+        if text == df.loc[i,'单词']:
+            print('Correct!')
+        else:
             results.append([df.loc[i,'单词'], df.loc[i,'解释']])
+            print(f"Incorrect!-->{df.loc[i,'单词']}")
         print('-'*50)
 
-# random model
-import random
-nums = []
 def rd():
+    nums = []
     length = len(df['No.'])
     for i in range(length):
         num = random.randint(0, length-1)
@@ -54,18 +51,21 @@ def rd():
     cnt = 1
     for i in nums:
         print(f"{cnt}. {df.loc[i, '解释']}")
-        text = input("Enter your response:\n")
+        fill_blanks(df.loc[i,'单词'])
+        text = input('Your response: ')
         df.loc[i, current_time] = text
-        if text != df.loc[i, '单词']:
+        if text == df.loc[i,'单词']:
+            print('Correct!')
+        else:
             results.append([df.loc[i,'单词'], df.loc[i,'解释']])
+            print(f"Incorrect!-->{df.loc[i,'单词']}")
         cnt += 1
         print('-'*50)
 
-# TEN model
-def ten():
+def ten(n):
     nums = []
     length = len(df['No.'])
-    for i in range(10):
+    for i in range(n):
         num = random.randint(0, length-1)
         while num in nums:
             num = random.randint(0, length-1)
@@ -73,29 +73,57 @@ def ten():
     cnt = 1
     for i in nums:
         print(f"{cnt}. {df.loc[i, '解释']}")
-        text = input("Enter your response:\n")
+        fill_blanks(df.loc[i,'单词'])
+        text = input('Your response: ')
         df.loc[i, current_time] = text
-        if text != df.loc[i, '单词']:
+        if text == df.loc[i,'单词']:
+            print('Correct!')
+        else:
             results.append([df.loc[i,'单词'], df.loc[i,'解释']])
+            print(f"Incorrect!-->{df.loc[i,'单词']}")
         cnt += 1
         print('-'*50)
 
-if model == 1:
-    order()
-elif model == 2:
-    rd()
-else:
-    ten()
+def model_choice():
+    models = {1:'Order', 2:'Random', 3:'I want X!'}
+    for k,v in models.items():
+        print(f"{k}: {v}")
+    num = int(input('Which model do you want to play: '))
+    if num == 1:
+        print('-'*50)
+        order()
+    elif num == 2:
+        print('-'*50)
+        rd()
+    elif num == 3:
+        n = int(input('How many words do you want to learn: '))
+        print('-'*50)
+        ten(n)
 
-# modify the original file
-df.to_excel(file, index=False)
+def fill_blanks(word):
+    test_word = ''
+    for letter in word:
+        test_word += random.choice(['_', letter])
+    print(test_word)
 
-if len(results) == 0:
-    print("Awesome! No wrong word!")
-else:
-    print("The following words are wrong:")
-    for i in range(len(results)):
-        print(f"{i+1}. {results[i][0]}, {results[i][1]}")
-    # generate a file that contain wrong words
-    res = pd.DataFrame(results, columns=['单词','解释'])
-    res.to_excel(f"{folder_path}{current_time}.xlsx",index=False)
+def main():
+    global results
+    results = []
+    profile()
+    global df
+    df = pd.read_excel(file_choice())
+    model_choice()
+
+    df.to_excel(file, index=False)
+    if len(results) == 0:
+        print("Awesome! No wrong word!")
+    else:
+        print("The following words are wrong:")
+        for i in range(len(results)):
+            print(f"{i+1}. {results[i][0]}, {results[i][1]}")
+        # generate a file that contain wrong words
+        res = pd.DataFrame(results, columns=['单词','解释'])
+        res.to_excel(f"{folder_path}{current_time}.xlsx",index=False)
+
+if __name__ == "__main__":
+    main()
